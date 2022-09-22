d6ec07a80f491b7e193639a04ac25873116df8c3
Ivan Kristiawan <ivanteens@gmail.com>
2022-09-22 16:04:13 +0700

    Add Binding

--- app/src/main/java/com/example/login/Register.kt
+++ app/src/main/java/com/example/login/Register.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 import android.view.View
 import android.widget.Toast
+import com.example.login.databinding.ActivityRegisterBinding
 import com.example.login.user.Constant
 import com.example.login.user.User
 import com.example.login.user.UserDB
@@ -16,6 +17,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class Register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     val db by lazy { UserDB(this) }
     private lateinit var username : TextInputEditText
     private lateinit var password : TextInputEditText
@@ -23,25 +25,28 @@ class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setTitle("")
 
         username = findViewById(R.id.etUsername)
         password = findViewById(R.id.etPassword)
-        btnRegister = findViewById(R.id.btnRegister)
 
-        btnRegister.setOnClickListener{
+        binding.btnRegister.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             val mBundle = Bundle()
-            mBundle.putString("username",username.text.toString())
-            mBundle.putString("password",password.text.toString())
+            mBundle.putString("username", username.text.toString())
+            mBundle.putString("password", password.text.toString())
             intent.putExtra("register", mBundle)
 
-            if(username.text.toString().length == 0){
+            if (username.text.toString().length == 0) {
                 CoroutineScope(Dispatchers.IO).launch {
                     db.noteDao().addNote(
-                        User(1, "a",
-                            "a")
+                        User(
+                            1, "a",
+                            "a"
+                        )
                     )
                     finish()
                 }
@@ -51,14 +56,78 @@ class Register : AppCompatActivity() {
 
             CoroutineScope(Dispatchers.IO).launch {
                 db.noteDao().addNote(
-                    User((Math.random() * (10000 - 100 + 1)).toInt(), username.text.toString(),
-                        password.text.toString())
+                    User(
+                        (Math.random() * (10000 - 3 + 1)).toInt(), username.text.toString(),
+                        password.text.toString()
+                    )
                 )
                 finish()
             }
 
             startActivity(intent)
         }
+        }
 
-    }
+//        btnRegister.setOnClickListener{
+//            val intent = Intent(this, MainActivity::class.java)
+//            val mBundle = Bundle()
+//            mBundle.putString("username",username.text.toString())
+//            mBundle.putString("password",password.text.toString())
+//            intent.putExtra("register", mBundle)
+//
+//            if(username.text.toString().length == 0){
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    db.noteDao().addNote(
+//                        User(1, "a",
+//                            "a")
+//                    )
+//                    finish()
+//                }
+//
+//                startActivity(intent)
+//            }
+//
+//            CoroutineScope(Dispatchers.IO).launch {
+//                db.noteDao().addNote(
+//                    User((Math.random() * (10000 - 3 + 1)).toInt(), username.text.toString(),
+//                        password.text.toString())
+//                )
+//                finish()
+//            }
+//
+//            startActivity(intent)
+//        }
+//        }
+
+//        btnRegister.setOnClickListener{
+//            val intent = Intent(this, MainActivity::class.java)
+//            val mBundle = Bundle()
+//            mBundle.putString("username",username.text.toString())
+//            mBundle.putString("password",password.text.toString())
+//            intent.putExtra("register", mBundle)
+//
+//            if(username.text.toString().length == 0){
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    db.noteDao().addNote(
+//                        User(1, "a",
+//                            "a")
+//                    )
+//                    finish()
+//                }
+//
+//                startActivity(intent)
+//            }
+//
+//            CoroutineScope(Dispatchers.IO).launch {
+//                db.noteDao().addNote(
+//                    User((Math.random() * (10000 - 3 + 1)).toInt(), username.text.toString(),
+//                        password.text.toString())
+//                )
+//                finish()
+//            }
+//
+//            startActivity(intent)
+//        }
+//
+//    }
 }
